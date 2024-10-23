2ab76f96696b6bc0143b28aa35228ecf8e4a872e
JurgenDeLira <arq.jorgedelira@gmail.com>
2024-10-22 23:13:46 -0600

    nuevo commit

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,23 @@
 # que el servidor va a poder manejar
 
 from flask_restful import Resource
-from flask import request #<- Nos permite interceptar la info del usuario
+from flask import request, render_template, make_response #<- Nos permite interceptar la info del usuario
 
 from .methods import user_register
 from .methods import user_login
 
+from flask_jwt_extended import jwt_required
 
 # Creamos un recurso que nuestra aplicación puede cargar (METODO)
 class HolaMundo(Resource):
   # Este método se ejecuta cuando el usuario lo llama con un GET
   def get(self):
-    return {'Mensaje': 'Hola mundo desde GET'}
+    pagina = render_template('index.html')
 
+    respuesta = make_response(pagina)
+
+    return respuesta
+  
   def post(self):
     return {'Mensaje': 'Hola mundo desde POST'}
 
@@ -49,6 +54,10 @@ class Login(Resource):
     return respuesta, status
 
 
+class Restringido(Resource): 
+  @jwt_required()#verificamos si mi usuario esta
+  def get(self):
+    return {'Mensaje': 'Felicidades estás loggeado'}
 
 # Simplemente se va a encargar de darle rutas a mis recursos
 class APIRoutes:
@@ -56,3 +65,4 @@ class APIRoutes:
     api.add_resource(HolaMundo, '/')
     api.add_resource(Registro, '/registro')
     api.add_resource(Login, '/login')
+    api.add_resource(Restringido, '/restringido')
